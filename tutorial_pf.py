312daf28a8c14ecba08857863ac9a6f769bea704
Gurashish Singh <gurashish1singh@gmail.com>
2022-03-12 14:53:23 -0500

    Updated file name and done with buttons and labels

--- tutorial_pf.py
+++ tutorial_pf.py
@@ -1,12 +1,17 @@
 import sys
 
+from PyQt6.QtCore import pyqtSlot as Slot
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import (
     QApplication, QLabel, QPushButton, QWidget
 )
 
+
 BUTTON_STYLE = (
     "background-color: #befef4"
+)
+CLICKED_ME = (
+    "background-color: #e9dc67"
 )
 WINDOW_STYLE = (
     "background-color: #f57f7f"
@@ -28,10 +33,15 @@ class Window(QWidget):
         button = QPushButton("Does this work?", self)
         button.setGeometry(100, 100, 99, 99)
         button.setStyleSheet(BUTTON_STYLE)
+        button.clicked.connect(self.clicked_it)
 
-        label = QLabel("Lets see if label works", self)
-        label.setGeometry(75, 201, 200, 50)
-        label.setFont(QFont("Cascadia Code"))
+        self.label = QLabel("Lets see if label works", self)
+        self.label.setGeometry(75, 201, 200, 50)
+        self.label.setFont(QFont("Cascadia Code"))
+
+    def clicked_it(self):
+        self.label.setText("Oh did you click me?")
+        self.label.setStyleSheet(CLICKED_ME)
 
 
 def create_application():
