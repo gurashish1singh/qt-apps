9af2e5e633fcc52c5918d577249bc49736df86b2
Gurashish Singh <gurashish1singh@gmail.com>
2022-03-12 13:03:16 -0500

    Added a basic button and label

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import (
+    QApplication, QLabel, QPushButton, QWidget
+)
 
-STYLE = (
+BUTTON_STYLE = (
+    "background-color: #befef4"
+)
+WINDOW_STYLE = (
     "background-color: #f57f7f"
 )
 
@@ -14,10 +20,22 @@ class Window(QWidget):
         print(self.__dir__())
         self.setWindowTitle("Basic App")
         self.setGeometry(500, 400, 300, 400)
-        self.setStyleSheet(STYLE)
+        self.setStyleSheet(WINDOW_STYLE)
+        self.create_widget()
+
+    def create_widget(self):
+        # Do not know why this takes self yet
+        button = QPushButton("Does this work?", self)
+        button.setGeometry(100, 100, 99, 99)
+        button.setStyleSheet(BUTTON_STYLE)
+
+        label = QLabel("Lets see if label works", self)
+        label.setGeometry(75, 201, 200, 50)
+        label.setFont(QFont("Cascadia Code"))
 
 
 def create_application():
+    # usually its sys.argv, need to read up on what it does
     app = QApplication([""])
     window = Window()
     window.show()
